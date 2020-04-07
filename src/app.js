4dfa7f96961b27e388aac5ce4e33f31835ff9ff1
Luciano Souza <luciano.souza@ifnmg.edu.br>
2020-04-07 15:07:58 -0300

    Adicionando validação de ID

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -9,6 +9,21 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+
+function validateRepositoryId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid repository Id.' })
+  }
+
+  return next();
+
+}
+
+//Para rotas que seguem esse padrao
+app.use('/repositories/:id', validateRepositoryId);
+
 
 app.get("/repositories", (request, response) => {
 
